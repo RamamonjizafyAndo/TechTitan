9880d95f9b952d26ea98b1bf1a14ffafa367ffaf
RamamonjizafyAndo <ramamonjizafymanitra06@gmail.com>
2023-12-16 12:28:19 +0300

    add config fireabse #master

---
+++ app/src/service/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2pRcBwycyqdU1l9vxzj5LTNA9hQYAqzE",
+  authDomain: "techtitan-352b1.firebaseapp.com",
+  projectId: "techtitan-352b1",
+  storageBucket: "techtitan-352b1.appspot.com",
+  messagingSenderId: "696253250565",
+  appId: "1:696253250565:web:4d7960a95a0bdc4be2b06c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
